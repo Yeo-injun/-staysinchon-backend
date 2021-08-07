8147cc2cc6297b0ef2e9af188bd08d40f4e46b84
Yeo-injun <youth.com11@gmail.com>
2021-08-07 17:05:05 +0900

    [GET] user/profile API 구현완료

--- src/main/java/com/project/sinchon/controller/UserController.java
+++ src/main/java/com/project/sinchon/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.project.sinchon.controller;
 
+import java.security.Principal;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +20,19 @@ import com.project.sinchon.config.security.auth.User;
 import com.project.sinchon.config.security.jwt.JwtTokenProvider;
 import com.project.sinchon.dao.UserDAO;
 import com.project.sinchon.dto.UserDTO;
+import com.project.sinchon.service.UserService;
 
 import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
+
+/*
+*
+* title : 사용자관련 Controller
+* author : 여인준
+* create date : 2021.04.21
+* update 
+* 2021.08.07 여인준 : [GET] user/profile API 구현 (스프링 시큐리티에서 권한 체크 후 principal객체 반환. 이 객체의 .getName() 메소드를 활용하여 userId값 가져오기
+* */
 
 @RequiredArgsConstructor // 변수 선언시 Null값을 넣어줘야 하는 것들에 자동으로 Null값 할당 
 @RestController
@@ -28,6 +41,9 @@ public class UserController {
 
 	@Autowired
 	private PrincipalDetailsService principalDetailsService;
+	
+	@Autowired
+	private UserService userService;
 	
 	// 비밀번호 암호화 처리 객체 선언 : SecurityConfig에 Bean으로 등록돼 있어야 함.
     private final BCryptPasswordEncoder passwordEncoder;
@@ -55,25 +71,15 @@ public class UserController {
     	return isDuplicate;
     }
     
-//    
-//    // 로그인
-//    @PostMapping("/login")
-//    public String login(@RequestBody Map<String, String> user) {
-//    	
-//    	// JwtAuthentication 클래스를 활용해서 수정작업해주기...! 21.05.15
-//        User authUser = customUserDetailService.loadUserByUsername(user.get("user_ID"));
-//        // 입력한 user_ID가 존재하지 않을 경우
-//        if (authUser == null) {
-//        	throw new IllegalArgumentException("가입되지 않은 회원 입니다.");
-//        }
-//       // 비밀번호가 틀릴 경우
-//        if (!passwordEncoder.matches(user.get("pwd"), authUser.getPwd())) {
-//            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-//        }
-//        System.out.println(authUser.getRole());
-//        System.out.println(authUser.getRoles());
-//        
-//        return jwtTokenProvider.createToken(authUser.getUser_ID(), authUser.getRoles());
-//         
-//    }
+    // 회원정보 보여주기 : 스프링 시큐리티에서 Auth체크를 진행하고, 통과하면 Principal객체를 반환함. 이 객체에 userID정보가 있어 이를 가져와서 회원정보를 가져오기
+    @GetMapping(value = "/profile", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public UserDTO getUserProfile(Principal principal) throws Exception {
+    	String userId = principal.getName();
+    	Map params = new HashMap();
+    	params.put("user_ID", userId);
+    	
+    	UserDTO result = userService.getUserDetails(params);
+    	return result;
+    }
+    
 }
